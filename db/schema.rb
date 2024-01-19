5b946368f659b46430bbfefa32e00d35b98a3d9d
binyamT <binyampowerhc@gmail.com>
2024-01-19 20:42:30 +0300

    refactor restriction for admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_045648) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_163553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_045648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
