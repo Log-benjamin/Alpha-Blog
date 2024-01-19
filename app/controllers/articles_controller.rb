5a304cddcdb3c502d1003a8052d1a3ae1cabe183
binyamT <binyampowerhc@gmail.com>
2024-01-19 18:35:02 +0300

    add reestriction for users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
-        # @articles = Article.all
         @articles = Article.paginate(page: params[:page], per_page: 5)
     end
 
